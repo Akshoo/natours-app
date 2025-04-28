17519ce829fa4d686dd4ae3bb041e75b7fd796c3
Akshoo <akshayshukla197@gmail.com>
2025-04-29 03:39:56 +0530

    stripe implementation try 4

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -62,9 +62,10 @@ const handlePaymentSuccess = catchAsync(async (session) => {
 	const tour = session.client_reference_id;
 	const user = (await User.find({ email: session.customer_email })).id;
 	const price = session.amount_total / 100;
-	console.log(session);
+	console.log(session, tour, user, price);
 	if (!tour && !user && !price) return next();
-
+    
+	console.log(tour, user, price);
 	await Booking.create({ tour, price, user });
 });
 export const getPaymentFail = (req, res, enxt) => {
